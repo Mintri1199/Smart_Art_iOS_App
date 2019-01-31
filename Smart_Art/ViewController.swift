480449b2d9fb1baff25d5897d07fb7fc6ef60def
Jackson Ho <mintri1199@gmail.com>
2019-01-31 12:25:49 -0800

    Initial Commit

---
+++ Smart_Art/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Smart_Art
+//
+//  Created by Jackson Ho on 1/31/19.
+//  Copyright © 2019 Jackson Ho. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
