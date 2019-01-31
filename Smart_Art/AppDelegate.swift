d48f0d0cc2764256a0c9098ff2cf8a6dcbe1b585
Jackson Ho <mintri1199@gmail.com>
2019-01-31 12:42:27 -0800

    initial commit and got rid of storyboard

--- Smart_Art/AppDelegate.swift
+++ Smart_Art/AppDelegate.swift
@@ -13,10 +13,19 @@ import CoreData
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
+    var navigationController: UINavigationController?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        window  = UIWindow(frame: UIScreen.main.bounds)
+        navigationController = UINavigationController(rootViewController: ViewController())
+        
+        
+        if let window = window{
+            window.rootViewController = navigationController
+            window.makeKeyAndVisible()
+        }
+        
         return true
     }
 
