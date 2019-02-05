56661ee51dd60cac8d28360380e4a5888eccb0c9
Jackson Ho <mintri1199@gmail.com>
2019-02-05 14:50:18 -0800

    Refactor logic

--- Smart_Art/fourButtonStackView.swift
+++ Smart_Art/fourButtonStackView.swift
@@ -18,18 +18,10 @@ class fourButtonStackView: UIStackView {
         addArrangedSubview(firstButtonRow)
         addArrangedSubview(secondButtonRow)
         translatesAutoresizingMaskIntoConstraints = false
-        
         firstButtonRow.leftButton.addTarget(self, action: #selector(answerButtonTapped(_:)), for: .touchUpInside)
-        firstButtonRow.leftButton.tag = 1
-        
         firstButtonRow.rightButton.addTarget(self, action: #selector(answerButtonTapped(_:)), for: .touchUpInside)
-        firstButtonRow.rightButton.tag = 2
-        
         secondButtonRow.leftButton.addTarget(self, action: #selector(answerButtonTapped(_:)), for: .touchUpInside)
-        secondButtonRow.leftButton.tag = 3
-        
         secondButtonRow.rightButton.addTarget(self, action: #selector(answerButtonTapped(_:)), for: .touchUpInside)
-        secondButtonRow.rightButton.tag = 4
     }
     
     required init(coder: NSCoder) {
@@ -54,35 +46,37 @@ class fourButtonStackView: UIStackView {
             if nextItemIndex[1] == mockQuestions.count{
                 endOfQuiz = true
             }
-            
             let answer = mockQuestions[index[0][1]][1]     // Holy shit what the fuck, Change this once the question model has been created
             if sender.titleLabel?.text == answer{
                 sender.backgroundColor = .green
                 quizVC.score += 1
                 quizVC.scoreLabel.text = "Score: \(quizVC.score)"
-                disableButtons(button: sender)
+                buttonsDisable()
                 if endOfQuiz{
                     print("End of quiz")
                 } else{
                     print("Correct Answer pressed")
                     Timer.scheduledTimer(timeInterval: 2, target: self, selector: #selector(delayTransition(timer:)), userInfo: nextIndex, repeats: false)
                 }
-                
             }else{
                 sender.backgroundColor = .red
+                buttonsDisable()
                 if endOfQuiz{
                     print("End of quiz")
                 } else{
                     print("Wrong Answer pressed")
-                    disableButtons(button: sender)
                     Timer.scheduledTimer(timeInterval: 2, target: self, selector: #selector(delayTransition(timer:)), userInfo: nextIndex, repeats: false)
                 }
             }
         }
     }
     
+    // Timer function after the interval
     @objc func delayTransition(timer: Timer){
+        // Since you can't pass argument in a selector, you have to utilizes useInfo to handle passing arguments
+        // Guard type cast userInfo because it is Any? by default
         guard let context = timer.userInfo as? [String : IndexPath] else { return }
+        // Then access the element you want
         let index = context["index", default: IndexPath(row: 0, section: 0)]
         
         if let quizVC = findViewController() as? ViewController {
@@ -91,35 +85,15 @@ class fourButtonStackView: UIStackView {
         }
     }
     
-    
-    // Disable other buttons once one is pressed
-    func disableButtons(button: AnswerButton) {
-        switch button.tag {
-        case 1:
-            firstButtonRow.rightButton.isEnabled = false
-            secondButtonRow.leftButton.isEnabled = false
-            secondButtonRow.rightButton.isEnabled = false
-        case 2:
-            firstButtonRow.leftButton.isEnabled = false
-            secondButtonRow.leftButton.isEnabled = false
-            secondButtonRow.rightButton.isEnabled = false
-        case 3:
-            firstButtonRow.rightButton.isEnabled = false
-            firstButtonRow.leftButton.isEnabled = false
-            secondButtonRow.rightButton.isEnabled = false
-        case 4:
-            firstButtonRow.rightButton.isEnabled = false
-            firstButtonRow.leftButton.isEnabled = false
-            secondButtonRow.leftButton.isEnabled = false
-            
-        default:
-            print("Error at switch statement")
-        }
-        // Prevent the correct button to be pressed more
-        button.isEnabled = false
+    // Disable all once one is pressed
+    func buttonsDisable() {
+        firstButtonRow.leftButton.isEnabled = false
+        firstButtonRow.rightButton.isEnabled = false
+        secondButtonRow.leftButton.isEnabled = false
+        secondButtonRow.rightButton.isEnabled = false
     }
     
-    // Re enable buttons after the cell move
+    // Re enable buttons after the cell moved
     func reEnableButton(){
         firstButtonRow.leftButton.isEnabled = true
         firstButtonRow.rightButton.isEnabled = true
